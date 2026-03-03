babb6c7a89446df068330f09af70ce6a8184a78e
kumarvishal7441 <kumarvishal7441@gmail.com>
2026-03-03 12:56:52 +0530

    add lazy loading for better loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
+import { lazy, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Notfound from './Pages/Notfound.jsx'
-import Cart from './Pages/Cart.jsx'
-import Checkout from './Pages/Checkout.jsx'
-import Home from './Pages/Home.jsx'
-import ProductDetail from './Pages/ProductDetail.jsx'
+// import Notfound from './Pages/Notfound.jsx'
+const Home = lazy(()=>import('./Pages/Home.jsx'));
+const Notfound = lazy(()=> import('./Pages/Notfound.jsx'));
+const ProductDetail = lazy(()=> import ('./Pages/ProductDetail.jsx'));
+const Cart = lazy(()=>import('./Pages/Cart.jsx'));
+const Checkout = lazy(()=>import('./Pages/Checkout.jsx'));
 
 const Routerpath = createBrowserRouter([
   {
